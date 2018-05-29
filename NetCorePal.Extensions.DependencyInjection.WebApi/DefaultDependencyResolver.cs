d8f6beef9fc0766cfce2f126a1429d2cac8e3fe4
wangdaobin <daobin.wang@xiaobao100.com>
2018-05-29 20:15:39 +0800

    添加项目文件。

---
+++ NetCorePal.Extensions.DependencyInjection.WebApi/DefaultDependencyResolver.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+
+namespace NetCorePal.Extensions.DependencyInjection.WebApi
+{
+    public class DefaultDependencyResolver : IDependencyResolver
+    {
+        protected IServiceProvider ServiceProvider;
+
+
+        public DefaultDependencyResolver(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new DefaultDependencyScope(ServiceProvider.CreateScope());
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return ServiceProvider.GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return ServiceProvider.GetServices(serviceType);
+        }
+    }
+}
---
+++ NetCorePal.Extensions.DependencyInjection.WebApi/DefaultDependencyScope.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+
+namespace NetCorePal.Extensions.DependencyInjection.WebApi
+{
+    public class DefaultDependencyScope : IDependencyScope
+    {
+        private readonly IServiceScope _serviceScope;
+
+        public DefaultDependencyScope(IServiceScope serviceScope)
+        {
+            _serviceScope = serviceScope;
+        }
+
+        public void Dispose()
+        {
+            _serviceScope.Dispose();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _serviceScope.ServiceProvider.GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _serviceScope.ServiceProvider.GetServices(serviceType);
+        }
+    }
+}
